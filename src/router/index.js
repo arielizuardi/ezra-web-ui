af02fa5bc1458225196f1b1eb6b609ead0a80c27
Arie <arie.lizuardi@gmail.com>
2017-05-02 17:46:47 +0700

    commit changes on may,2 2017

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,9 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import GSheetImporter from '@/components/GSheetImporter'
+import ReportPage from '@/components/ReportPage'
+import { Line } from 'vue-chartjs'
+
 
 
 Vue.use(Router)
@@ -11,6 +14,10 @@ export default new Router({
     {
       path: '/',
       component: GSheetImporter
+    },
+    {
+      path: '/report',
+      component: ReportPage
     }
   ]
 })
